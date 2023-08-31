37ad18e646ad39a20e9d3766fc713646cabf1b17
Yersson-Benavides <111075359+Yersson-Benavides@users.noreply.github.com>
2023-08-31 16:32:43 -0500

    Nuevo APPWEB

--- AppWeb.py
+++ AppWeb.py
@@ -9,16 +9,14 @@ from dash.dependencies import Input, Output, State
 
 import dash_bootstrap_components as dbc
 
-import dash
-from dash import dcc
-from dash import html
-from dash.dependencies import Input, Output
-import pandas as pd
+# from dash import Input, Output, State
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
-import dash_bootstrap_components as dbc
+
+import plotly.express as px
+
 
 
 df20 = pd.read_csv('datosLimpios2020.csv')
@@ -44,8 +42,10 @@ opciones_jornada = data_Frame['JORNADA'].unique()
 opciones_dia_semana = data_Frame['DIA_SEMANA'].unique()
 opciones_barrio_corregimiento_via = data_Frame['BARRIO-CORREGIMIENTO-VIA'].unique()
 
-app = dash.Dash(__name__, suppress_callback_exceptions=True, external_stylesheets=[dbc.themes.BOOTSTRAP])
-server=app.server
+#app = dash.Dash(__name__, suppress_callback_exceptions=True, external_stylesheets=[dbc.themes.BOOTSTRAP])
+app = dash.Dash(__name__, suppress_callback_exceptions=True, external_stylesheets=[dbc.themes.BOOTSTRAP, "/assets/style.css"])
+server = app.server
+
 def generate_map(df20, df21, df22):
     feature_gp1 = folium.FeatureGroup(name="2020")
     feature_gp2 = folium.FeatureGroup(name="2021")
@@ -91,16 +91,106 @@ d4 = a4.unstack()
 a5 = UDts.groupby(['GENERO', 'AÑO'])['GENERO'].count()
 d5 = a5.unstack()
 
-app.layout = dbc.Container(
-    fluid=True,
-    children=[
+
+#Grafico frecuencia de accidentes
+
+# Paso 1: Cargar y preparar los datos
+UDts3 = UDts.copy()
+UDts3["HORA"] = UDts3["HORA"].replace({'20:00:00 p: m:':'20:00'}, regex=True)
+UDts3["HORA"] = UDts3["HORA"].replace({'NOCHE':'20:00'}, regex=True)
+UDts3["HORA"] = UDts3["HORA"].replace({'1900-08-17T00:00':'20:00'}, regex=True)
+unique_dates = set(UDts3['FECHA'])
+unique_times = set(UDts3['HORA'])
+
+# Paso 2: Combinar las columnas de fecha y hora
+UDts3['FECHAHORA'] = pd.to_datetime(UDts3["FECHA"] + " " + UDts3["HORA"])
+
+# Paso 3: Agrupación temporal
+UDts3 = UDts3.set_index('FECHAHORA')
+data_daily = UDts3.resample('D').size()
+
+# Crear figuras para las gráficas
+figs = []
+
+for year in [2020, 2021, 2022]:
+    data_year = data_daily[data_daily.index.year == year]
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=data_year.index, y=data_year.values, mode='lines', name=f'Año {year}'))
+    fig.update_layout(title=f'Serie temporal de accidentes de tráfico - {year}',
+                      xaxis_title='Fecha', yaxis_title='Número de accidentes')
+    figs.append(fig)
+
+
+#Grafico pastel Gravedad, Jornada y Año
+
+UDts4 = UDts.copy()
+data_dict = {}
+for year in UDts4['AÑO'].unique():
+    data_dict[year] = UDts4[UDts4['AÑO'] == year]['GRAVEDAD'].value_counts()
+
+data_df = pd.DataFrame(data_dict)
+
+app.layout = html.Div(
+    [
+        html.Link(rel='stylesheet', href='/assets/style.css'),
+        dbc.Navbar(
+            children=[
+                html.A(
+                    dbc.Row(
+                        [
+                            dbc.Col(html.Img(src="/assets/logo.png", height="50px")),
+                            dbc.Col(dbc.NavbarBrand("Diplomado en Ciencia de Datos 'Tomando decisiones basadas en conocimiento'", className="ml-2")),
+                        ],
+                        align="center",
+                    ),
+                    href="/",
+                )
+            ],
+            className=' NavbarFondo',
+            style={'fontWeight': 'bold', 'fontWeight': '600', 'backgroundColor': 'rgba(255, 255, 255, 0.5)'}
+        ),
+        
+        dbc.Container(
+            fluid=True,
+            children=[
+        
         dbc.Row(
             dbc.Col(
                 [
-                    dcc.Markdown('## Análisis de accidentes de tránsito'),
-                    dcc.Markdown('En esta página encontrarás un análisis de los accidentes de tránsito ocurridos en los años 2020, 2021 y 2022, así como una posible predicción para el año 2023 en base a los datos obtenidos.')
-                ]
+                    dcc.Markdown('# Análisis de accidentes de tránsito'),
+                    dcc.Markdown('#### En esta página encontrarás un análisis de los accidentes de tránsito ocurridos en los años 2020, 2021 y 2022, así como la predicción de la gravedad de los mismos de acuerdo a variables como el Barrio, la Jornada, y el Día de la Semana donde ocurrirá el accidente.', style={'marginTop':'50px'})
+                ],
+                className= 'FondoTitulos'
+                
             )
+        ),
+            ],
+            className = 'fondo container-fluid'
+           
+        ),
+        
+        dbc.Container(
+            fluid=True,
+            children=[
+        
+        dbc.Row(
+            [
+                dbc.Col(
+                    [
+                        dbc.Card(
+                            [
+                                dbc.CardHeader("A continuación puedes acceder a un modelo de predicción haciendo clic en el botón de abajo"),
+                                dbc.CardBody(
+                                    [
+                                        dbc.Button('Ir a predicción', id='btn-prediction', n_clicks=0),
+                                        html.Div(id='prediction-modal')
+                                    ]
+                                ),
+                            ]
+                        ),
+                    ]
+                )
+            ]
         ),
 
         dbc.Row(
@@ -208,35 +298,21 @@ app.layout = dbc.Container(
         dbc.Row(
             [
                 dbc.Col(
+    [
+        dbc.Card(
+            [
+                dbc.CardHeader("Gráfico de accidentes de transito por año"),
+                dbc.CardBody(
                     [
-                        dbc.Card(
-                            [
-                                dbc.CardHeader("Gráficas de accidentes por clase de vehículo en cada año"),
-                                dbc.CardBody(
-                                    [
-                                        dcc.Graph(
-                                            id='grafica-vehiculo',
-                                            figure={
-                                                'data': [
-                                                    go.Bar(x=d4.index, y=d4[2020], name='2020'),
-                                                    go.Bar(x=d4.index, y=d4[2021], name='2021'),
-                                                    go.Bar(x=d4.index, y=d4[2022], name='2022')
-                                                ],
-                                                'layout': go.Layout(
-                                                    title='Accidentes por clase de vehículo en cada año',
-                                                    xaxis={'title': 'Clase de vehículo'},
-                                                    yaxis={'title': 'Cantidad de accidentes'},
-                                                    barmode='group'
-                                                )
-                                            }
-                                        )
-                                    ]
-                                ),
-                            ]
-                        ),
-                    ],
-                    width=6
+                        dcc.Graph(figure=fig) for fig in figs
+                    ]
                 ),
+            ]
+        ),
+    ],
+    width=6
+),
+
 
                 dbc.Col(
                     [
@@ -244,72 +320,162 @@ app.layout = dbc.Container(
                             [
                                 dbc.CardHeader("Gráficas de accidentes por género en cada año"),
                                 dbc.CardBody(
-                                    [
-                                        dcc.Graph(
-                                            id='grafica-genero',
-                                            figure={
-                                                'data': [
-                                                    go.Bar(x=d5.index, y=d5[2020], name='2020'),
-                                                    go.Bar(x=d5.index, y=d5[2021], name='2021'),
-                                                    go.Bar(x=d5.index, y=d5[2022], name='2022')
-                                                ],
-                                                'layout': go.Layout(
-                                                    title='Accidentes por género en cada año',
-                                                    xaxis={'title': 'Género'},
-                                                    yaxis={'title': 'Cantidad de accidentes'},
-                                                    barmode='group'
-                                                )
-                                            }
-                                        )
-                                    ]
-                                ),
+    [
+        dcc.Graph(
+            id='grafica-genero',
+            figure={
+                'data': [
+                    go.Bar(x=d5.index, y=d5[2020], name='2020'),
+                    go.Bar(x=d5.index, y=d5[2021], name='2021'),
+                    go.Bar(x=d5.index, y=d5[2022], name='2022')
+                ],
+                'layout': go.Layout(
+                    title='Accidentes por género en cada año',
+                    xaxis={'title': 'Género'},
+                    yaxis={'title': 'Cantidad de accidentes'},
+                    barmode='group'
+                )
+            }
+        )
+    ]
+),
                             ]
                         ),
-                    ],
-                    width=6
+                        
+                        dbc.Card(
+                            [
+                                dbc.CardHeader("Gráfico de gravedad Vs Jornada por año"),
+                            dbc.CardBody(
+                                [
+                                    dcc.Dropdown(
+                                        id='year-dropdown',
+                                        options=[{'label': year, 'value': year} for year in data_df.index],
+                                        value=data_df.index[0]
+                                    ),
+                                    dcc.Graph(id='pie-chart'),
+                                ]
+                            ),
+                        ]
+                    ),
+                ],
+                width=6
+                    
                 ),
             ]
         ),
 
+        
+    
+            ],
+            className="container-fluid",
+            style={"padding": "50px", "backgroundColor": "white"},
+        ),
         dbc.Row(
-            [
-                dbc.Col(
                     [
-                        dbc.Card(
+                        
+                        dbc.Col(
+                    [
+                        html.Div(
                             [
-                                dbc.CardHeader("A continuación puedes acceder a un modelo de predicción haciendo clic en el botón de abajo"),
-                                dbc.CardBody(
+                                html.A(
                                     [
-                                        dbc.Button('Ir a predicción', id='btn-prediction', n_clicks=0),
-                                        html.Div(id='prediction-modal')
-                                    ]
+                                        html.Img(src="/assets/linkedin.png", width="30px"),
+                                        html.Div(
+                                            [
+                                                html.P("Linkedin", className="social-media-text"),
+                                            ],
+                                            className="social-media-icon-text"
+                                        )
+                                    ],
+                                    href="https://www.linkedin.com/in/yersson-benavides/",
+                                    target="_blank",
+                                    className="social-media-icon-link"
                                 ),
-                            ]
+                                html.A(
+                                    [
+                                        html.Img(src="/assets/github.png", width="30px"),
+                                        html.Div(
+                                            [
+                                                html.P("GitHub", className="social-media-text"),
+                                            ],
+                                            className="social-media-icon-text"
+                                        )
+                                    ],
+                                    href="https://github.com/Yersson-Benavides/Diplomado_Prediccion_Gravedad_Accidentes_Transito_Palmira",
+                                    target="_blank",
+                                    className="social-media-icon-link"
+                                ),
+                            ],
+                            
+                            className="social-media-icons",
+                        )
+                    ],
+                    width=12,
+                    #     dbc.Col(
+                    # [
+                    #     html.Div(
+                    #         [
+                    #             html.A(
+                    #                 [
+                    #                     html.Img(src="/assets/linkedin.png"),
+                    #                     html.P("Linkedin", className="social-media-text"),
+                    #                 ],
+                    #                 href="https://www.linkedin.com/in/yersson-benavides/",
+                    #                 target="_blank",
+                    #             ),
+                    #             html.A(
+                    #                 [
+                    #                     html.Img(src="/assets/github.png"),
+                    #                     html.P("GitHub", className="social-media-text"),
+                    #                 ],
+                    #                 href="https://github.com/Yersson-Benavides/Diplomado_Prediccion_Gravedad_Accidentes_Transito_Palmira",
+                    #                 target="_blank",
+                    #             ),
+                    #         ],
+                    #         className="social-media-icons",
+                    #     )
+                    # ],
+                    # width=12,
                         ),
-                    ]
-                )
-            ]
-        )
-    ],
-    className='container-fluid',
-    style={'padding': '50px','backgroundColor': 'white'}
+                    ],
+                    className="footer",
+                ),
+    ]
 )
+
+
+@app.callback(
+    Output('pie-chart', 'figure'),
+    [Input('year-dropdown', 'value')]
+)
+
+def update_pie_chart(selected_year):
+    fig = px.pie(
+        data_frame=data_df.loc[selected_year],
+        names=data_df.columns,
+        values=selected_year,
+        title=f'Distribución de Gravedad por Tipo de Jornada - {selected_year}',
+        labels={'index': 'Gravedad'}
+    )
+    return fig
 
 @app.callback(
     Output('prediction-modal', 'children'),
     [Input('btn-prediction', 'n_clicks')]
 )
 
+
 def open_prediction_modal(n_clicks):
     if n_clicks:
         return dbc.Modal(
             [
-                dbc.ModalHeader("Realizar predicciones de accidentes de tránsito"),
+                
                 dbc.ModalBody(
                     [
-                        html.P('Aquí puedes realizar predicciones de accidentes de tránsito utilizando un modelo predictivo.'),
+                        
                         # Prediccion arbol de desición
-                        html.H1("Realizar predicciones de accidentes de tránsito"),
+                        html.H2("Realizar predicciones de gravedad en accidentes de transito dentro de Palmira"),
+                        html.P('Aquí puedes realizar la predicción de la gravedad de un accidente de transito en el la ciudad de Palmira utilizando un modelo predictivo.'),
                         html.Div(
                             [
                                 html.P('Jornada:'),
@@ -364,54 +530,48 @@ def open_prediction_modal(n_clicks):
 )
 def make_prediction(n_clicks, jornada, dia_semana, barrio_corregimiento_via):
     if n_clicks:
-        if jornada not in opciones_jornada or dia_semana not in opciones_dia_semana or barrio_corregimiento_via not in opciones_barrio_corregimiento_via:
+        if (jornada not in opciones_jornada or
+            dia_semana not in opciones_dia_semana or
+            barrio_corregimiento_via not in opciones_barrio_corregimiento_via):
             return html.P("No has ingresado una opción válida. Por favor, inténtalo nuevamente.")
-        
+
         # Preparar los datos del usuario
         user_data = pd.DataFrame({
             'JORNADA': [jornada],
             'DIA_SEMANA': [dia_semana],
             'BARRIO-CORREGIMIENTO-VIA': [barrio_corregimiento_via]
         })
-        
+
         # Codificar las variables categóricas en el conjunto de datos completo
         encoder = OneHotEncoder(sparse=False, handle_unknown='ignore')
         data_Frame_encoded = encoder.fit_transform(data_Frame[['JORNADA', 'DIA_SEMANA', 'BARRIO-CORREGIMIENTO-VIA']])
-        
+
         # Obtener las columnas correspondientes a los datos del usuario
         user_data_encoded = encoder.transform(user_data)
-        
+
         # Dividir los datos en características (X) y objetivo de predicción (y)
         X = data_Frame_encoded
         y = LabelEncoder().fit_transform(data_Frame['GRAVEDAD'])
-        
-        # Dividir los datos en conjuntos de entrenamiento y prueba
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-        
-        # Entrenar el modelo Árbol de Decisión
+
+        # Entrenar el modelo Árbol de Decisión con el 100% de los datos
         model = DecisionTreeClassifier()
-        model.fit(X_train, y_train)
-        
+        model.fit(X, y)
+
         # Realizar la predicción con los datos del usuario
         prediction = model.predict(user_data_encoded)
-        
+
         # Inicializar un nuevo codificador de etiquetas y ajustarlo con los datos de gravedad
         gravity_encoder = LabelEncoder()
         gravity_encoder.fit(data_Frame['GRAVEDAD'])
-        
+
         # Obtener la etiqueta correspondiente a la predicción de gravedad
         predicted_gravity = gravity_encoder.inverse_transform(prediction)
-        
-        # Calcular la precisión del modelo en el conjunto de prueba
-        y_pred = model.predict(X_test)
-        precision = accuracy_score(y_test, y_pred)
-        precision_percentage = precision * 100
-        
+
         return html.Div([
-            html.P("Predicción de gravedad: {}".format(predicted_gravity[0])),
-            html.P("Precisión del modelo: {:.2f}%".format(precision_percentage))
+            html.P("Predicción de gravedad: {}".format(predicted_gravity[0]))
         ])
 
+# Callback para abrir y cerrar el modal de predicción
 @app.callback(
     Output('modal', 'is_open'),
     [Input('btn-prediction', 'n_clicks'),
